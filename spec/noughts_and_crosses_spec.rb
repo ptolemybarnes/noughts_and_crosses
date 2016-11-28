77fbb6714e91a2dbafe7bca82608b5a70991571c
Ptolemy <ptolemybarnes@gmail.com>
2016-11-28 22:33:01 +0000

    A nought followed by a cross can be placed on the board

--- spec/noughts_and_crosses_spec.rb
+++ spec/noughts_and_crosses_spec.rb
@@ -1,13 +1,56 @@
 module NoughtsAndCrosses
 
-  describe 'A game of noughts and crosses' do
+  class Game
 
+    LOCATIONS = [
+      :top_left, :top_middle, :top_right, :middle_left, :middle, :middle_right,
+      :bottom_left, :bottom_middle, :bottom_right
+    ]
+
+    def initialize
+      @moves = []
+    end
+
+    def place_nought_at(location)
+      raise RuleViolationError if !moves.empty? && moves.last[1] == "0"
+      moves << [location, "0"]
+    end
+
+    def place_cross_at(location)
+      moves << [location, "X"]
+    end
+
+    def print_grid
+        "-----\n|" +
+        _print_grid +
+        "|\n-----\n"
+    end
+
+    private
+
+    def _print_grid
+      moves_and_marks = moves.to_h
+      LOCATIONS.map do |location|
+        moves_and_marks.fetch(location, " ")
+      end.each_slice(3).to_a.map(&:join).join("|\n|")
+    end
+
+    attr_reader :moves
+  end
+
+  class RuleViolationError < StandardError; end
+end
+
+
+module NoughtsAndCrosses
+
+  describe Game do
     it 'placing a 0 on the grid' do
       game = Game.new
-      game.place_nought_at(:center)
+      game.place_nought_at(:middle)
 
       expect(game.print_grid).to eq(<<~EXAMPLE
-        _____
+        -----
         |   |
         | 0 |
         |   |
@@ -15,28 +58,28 @@ module NoughtsAndCrosses
       EXAMPLE
       )
     end
-  end
 
-end
+    it 'placing a 0 followed by an X' do
+      game = Game.new
+      game.place_nought_at(:middle)
+      game.place_cross_at(:middle_left)
 
-module NoughtsAndCrosses
-
-  class Game
-
-    def place_nought_at(location)
-    end
-
-    def print_grid
-      <<~EXAMPLE
-        _____
+      expect(game.print_grid).to eq(<<~EXAMPLE
+        -----
         |   |
-        | 0 |
+        |X0 |
         |   |
         -----
       EXAMPLE
+      )
     end
 
+    it "doesn't allow two 0s to be placed consecutively" do
+      game = Game.new
+      game.place_nought_at(:middle)
+
+      expect { game.place_nought_at(:top_right) }.to raise_error(RuleViolationError)
+    end
   end
 
 end
-
