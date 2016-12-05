f3e43674d62fe4b31af8b56aa1e863513628245d
Ptolemy <ptolemybarnes@gmail.com>
2016-12-05 17:46:25 +0000

    Refactors MoveDecision class

--- lib/move_decision.rb
+++ lib/move_decision.rb
@@ -14,28 +14,27 @@ module NoughtsAndCrosses
       winning_move_for(mark) || blocking_move_for(mark) || Move.new(Point.new(0, 2), mark)
     end
 
+    private
+
     def blocking_move_for(mark)
-      opponent_winning_move_location = winning_move_location_for(other_mark(mark))
-      return nil if opponent_winning_move_location.nil?
-      Move.new(opponent_winning_move_location, mark)
+      opponent_winning_move_point = winning_move_point_for(other_mark(mark))
+      Move.new(opponent_winning_move_point, mark) if opponent_winning_move_point
     end
 
     def winning_move_for(mark)
-      winning_move_point = winning_move_location_for(mark)
-      return false if winning_move_point.nil?
-      Move.new(winning_move_point, mark)
+      winning_move_point = winning_move_point_for(mark)
+      Move.new(winning_move_point, mark) if winning_move_point
     end
 
-    def winning_move_location_for(mark)
+    def winning_move_point_for(mark)
       winning_line = grid.find_line do |line|
-        line.select do |move|
+        line.count do |move|
           move.mark == mark
-        end.length == 2 && line.none? {|move| move.mark == other_mark(mark) }
+        end == 2 && line.none? {|move| move.mark == other_mark(mark) }
       end
       return nil if winning_line.nil?
       winning_line.find {|move| move.mark.null_mark? }.point
     end
-    private
 
     def other_mark(mark)
       mark == Nought ? Cross : Nought
