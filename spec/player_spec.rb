cb73bd7b805185ac0fc39709be9807202e1d087d
Ptolemy <ptolemybarnes@gmail.com>
2016-12-09 18:13:20 +0000

    Refactors player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,35 +1,49 @@
+require 'pry'
 module NoughtsAndCrosses
   describe 'Players' do
-    let(:input_channel)  { instance_double(IO) }
+    let(:input_channel)  { instance_double(IO, gets: '1, 1') }
     let(:output_channel) { instance_double(IO, puts: nil) }
     let(:grid) { instance_double(Grid, print: '') }
 
+    before do
+      allow(output_channel).to receive(:puts)
+    end
+
     describe HumanPlayer do
+      let(:human_player) do
+        HumanPlayer.new(Nought, input: input_channel, output: output_channel)
+      end
+
       it "takes the coordinates of a point and creates a move" do
         allow(input_channel).to receive(:gets).and_return('0, 0')
 
-        player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
-
-        expect(player.get_move(grid)).to eq Move.new(Point.new(0, 0), Nought)
+        expect(human_player.get_move(grid)).to eq Move.new(Point.new(0, 0), Nought)
       end
 
-      it "checks that the input received is a possible point location" do
+      it "validates that the input received is a possible point location" do
         allow(input_channel).to receive(:gets).and_return('0')
 
-        player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
+        expect { human_player.get_move(grid) }.to raise_error InvalidInputError
+      end
 
-        expect { player.get_move(grid) }.to raise_error InvalidInputError
+      it 'prints the grid and sends it to the output channel' do
+        allow(grid).to receive(:print).and_return(:grid_representation)
+
+        human_player.get_move(grid)
+
+        expect(output_channel).to have_received(:puts).with(:grid_representation)
       end
     end
 
     describe ComputerPlayer do
+      let(:computer_player) do
+        ComputerPlayer.new(Nought, input: input_channel, output: output_channel)
+      end
 
       it "gets a move from MoveDecision" do
         allow(MoveDecision).to receive(:sample).with(grid, Nought).and_return(:move)
 
-        player = ComputerPlayer.new(Nought, input: input_channel, output: output_channel)
-
-        expect(player.get_move(grid)).to eq :move
+        expect(computer_player.get_move(grid)).to eq :move
       end
     end
   end
