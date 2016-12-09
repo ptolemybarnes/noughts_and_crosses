19590365cb446697bd644ea5409471762979b51c
Ptolemy <ptolemybarnes@gmail.com>
2016-12-09 17:57:53 +0000

    Tests ComputerPlayer

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,28 +1,36 @@
 module NoughtsAndCrosses
-  describe HumanPlayer do
-
+  describe 'Players' do
     let(:input_channel)  { instance_double(IO) }
     let(:output_channel) { instance_double(IO, puts: nil) }
     let(:grid) { instance_double(Grid, print: '') }
 
-    it "takes the coordinates of a point and creates a move" do
-      allow(input_channel).to receive(:gets).and_return('0, 0')
+    describe HumanPlayer do
+      it "takes the coordinates of a point and creates a move" do
+        allow(input_channel).to receive(:gets).and_return('0, 0')
 
-      player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
+        player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
 
-      expect(player.get_move(grid)).to eq Move.new(Point.new(0, 0), Nought)
+        expect(player.get_move(grid)).to eq Move.new(Point.new(0, 0), Nought)
+      end
+
+      it "checks that the input received is a possible point location" do
+        allow(input_channel).to receive(:gets).and_return('0')
+
+        player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
+
+        expect { player.get_move(grid) }.to raise_error InvalidInputError
+      end
     end
 
-    it "checks that the input received is a possible point location" do
-      allow(input_channel).to receive(:gets).and_return('0')
+    describe ComputerPlayer do
 
-      player = HumanPlayer.new(Nought, input: input_channel, output: output_channel)
+      it "gets a move from MoveDecision" do
+        allow(MoveDecision).to receive(:sample).with(grid, Nought).and_return(:move)
 
-      expect { player.get_move(grid) }.to raise_error InvalidInputError
+        player = ComputerPlayer.new(Nought, input: input_channel, output: output_channel)
+
+        expect(player.get_move(grid)).to eq :move
+      end
     end
-  end
-
-  describe ComputerPlayer do
-
   end
 end
