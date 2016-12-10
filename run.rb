fbb34da53acb3dcce1e82883bcc3cc2e9f533328
Ptolemy <ptolemybarnes@gmail.com>
2016-12-10 11:02:30 +0000

    Script for running the game from the command line

--- run.rb
+++ run.rb
@@ -5,40 +5,67 @@ require './lib/grid'
 require './lib/mark'
 require './lib/moves_list'
 require './lib/move'
-require './lib/move_decision'
 require './lib/move_decision_strategy.rb'
+require './lib/move_decision'
+require './lib/player'
 require './lib/line'
 
-# COMPUTER GOES SECOND
-# this script randomly generates moves vs the computer to ensure it always wins when it can
-# v messy!
-
+# this script runs the game from the command-line
 include NoughtsAndCrosses
-game  = Game.new
-count = 1
-turn  = Nought
-puts game.print
 
-loop do
-  until game.over?
-    turn = (turn == Nought ? Cross : Nought)
-    if turn == Nought
-      move = MoveDecision.make(game.grid, turn)
-    else
-      available_moves = game.grid.cells.select {|move| move.mark.null_mark? }
-      point = Point.new(*gets.chomp.split(' ').map(&:to_i))
-      move = Move.new(point, turn)
-    end
-    game.play(move)
-    puts game.print
+class Prompt
+  def self.gets
+    print "\n=> "
+    STDIN.gets
   end
-  puts "#{count} :: GAME OVER!"
-  count += 1
-  winning_line = game.send(:winning_line)
-  if (winning_line && winning_line.first.mark == Cross) || (winning_line.nil? && game.grid.fetch(Point.middle).mark != Cross)
-    fail 'Computer lost!'
-  end
-  game = Game.new
-  turn = Nought
 end
 
+MODES = {
+  'Human vs Human'       => [ HumanPlayer.new(Nought, input: Prompt), HumanPlayer.new(Cross, input: Prompt) ],
+  'Human vs Computer'    => [ HumanPlayer.new(Nought, input: Prompt), ComputerPlayer.new(Cross) ],
+  'Computer vs Computer' => [ ComputerPlayer.new(Nought), ComputerPlayer.new(Cross) ]
+}
+
+MARKS = {
+  "Nought starts" => Nought,
+  "Cross starts"  => Cross
+}
+
+def present_selection_for(options)
+  selection_number = ""
+  options_range = (0..options.length)
+  is_selection_in_options = Regexp.new(/^[#{ options_range.map(&:to_s).join }]$/)
+  until selection_number.match(is_selection_in_options)
+    puts "Enter a number\n\n"
+    options.each_with_index do |mode, index|
+      puts "#{index + 1}. #{mode}"
+    end
+    selection_number = Prompt.gets.chomp
+  end
+  selection_number = selection_number.to_i - 1
+  puts "\nYou chose '#{options[selection_number]}'\n"
+  selection_number
+end
+
+game_mode_number = present_selection_for(MODES.keys)
+players = MODES.values[game_mode_number].cycle
+
+starting_mark_number = present_selection_for(MARKS.keys)
+starting_mark = MARKS.values[starting_mark_number]
+players.next if starting_mark == Cross
+
+game = Game.new
+puts 'Game starting!'
+until game.over?
+  begin
+    game.play(players.peek.get_move(game.grid))
+    players.next
+  rescue Player::RuleViolationError => error
+    puts "You cannot enter the move '#{error}'"
+  rescue InvalidInputError => error
+    puts "The move '#{error}' was invalid. Please enter a move 'x, y'."
+  end
+end
+
+puts game.print
+puts 'Game over!'
